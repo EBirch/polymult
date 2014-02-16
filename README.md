430910a317b8ddc2f83d162b5b40feb04aae90b7
ebirch <ethanjbirch@gmail.com>
2014-02-15 22:10:09 -0700

    All algorithms completed, timing added

---
+++ README.md
@@ -0,0 +1,11 @@
+##polymult
+========
+
+Polynomial multiplication algorithms for CS 5050
+
+Run command:
+
+./polymult -[d|3|4] n
+
+The first option selects the algorithm to run with (dumb, 4 multiplication, or 3 multiplication)
+The second option sets the problem size (number of coefficients per polynomial)
--- main.cpp
+++ main.cpp
@@ -5,35 +5,37 @@
 #include <ctime>
 #include <iostream>
 
-std::vector<float> dumbMultiply(std::vector<float> poly1, std::vector<float> poly2);
+std::vector<float> dumbMult(std::vector<float> poly1, std::vector<float> poly2);
 std::vector<float> fourMult(std::vector<float> poly1, std::vector<float> poly2);
+std::vector<float> threeMult(std::vector<float> poly1, std::vector<float> poly2);
 
 int main(int argc, char **argv){
-	// if(argc < 2){
-	// 	std::cout<<"Missing problem size argument\n";
-	// 	return -1;
-	// }
-	// int POLYTERMS = atoi(argv[1]);
-	// std::mt19937 rng(time(NULL));
-	// std::uniform_real_distribution<> dist(-1, 1);
-	// std::vector<float> poly1(POLYTERMS), poly2(POLYTERMS);
-	// std::generate(poly1.begin(), poly1.end(), [&](){return dist(rng);});
-	// std::generate(poly2.begin(), poly2.end(), [&](){return dist(rng);});
-	std::vector<float> poly1 {1, -2, 3, 8};
-	std::vector<float> poly2 {1, 2, 2, 1};
-	auto temp = dumbMultiply(poly1, poly2);
-	for(auto &term : temp){
-		std::cout<<term<<" ";
+	if(argc < 3){
+		std::cout<<"Not enough arguments"<<std::endl;
+		return -1;
 	}
-	std::cout<<std::endl;
-	temp = fourMult(poly1, poly2);
+	int POLYTERMS = atoi(argv[2]);
+	std::mt19937 rng(time(NULL));
+	std::uniform_real_distribution<> dist(-1, 1);
+	std::vector<float> poly1(POLYTERMS), poly2(POLYTERMS);
+	std::generate(poly1.begin(), poly1.end(), [&](){return dist(rng);});
+	std::generate(poly2.begin(), poly2.end(), [&](){return dist(rng);});
+	std::vector<float> temp;
+	auto start = std::chrono::steady_clock::now();
+	switch(argv[1][1]){
+		case 'd': temp = dumbMult(poly1, poly2); break;
+		case '4': temp = fourMult(poly1, poly2); break;
+		case '3': temp = threeMult(poly1, poly2); break;
+	}
+	std::cout<<"Ran in "<<(std::chrono::duration_cast<std::chrono::duration<float>>(std::chrono::steady_clock::now() - start).count())<<" seconds"<<std::endl;
+	std::cout<<"Resulting coefficients: ";
 	for(auto &term : temp){
 		std::cout<<term<<" ";
 	}
 	std::cout<<std::endl;
 }
 
-std::vector<float> dumbMultiply(std::vector<float> poly1, std::vector<float> poly2){
+std::vector<float> dumbMult(std::vector<float> poly1, std::vector<float> poly2){
 	std::vector<float> result(2 * poly1.size() - 1, 0);
 	for(int i = 0; i < poly1.size(); ++i){
 		for(int j = 0; j < poly2.size(); ++j){
@@ -65,6 +67,37 @@ std::vector<float> fourMult(std::vector<float> poly1, std::vector<float> poly2){
 		result[poly1.size() / 2 + i] += temp[i];
 	}
 	temp = fourMult(p1High, p2Low);
+	for(int i = 0; i < temp.size(); ++i){
+		result[poly1.size() / 2 + i] += temp[i];
+	}
+	return result;
+}
+
+std::vector<float> threeMult(std::vector<float> poly1, std::vector<float> poly2){
+		if(poly1.size() == 1){
+		return std::vector<float> {poly1[0] * poly2[0]};
+	}
+	std::vector<float> p1Low(&poly1[0], &poly1[poly1.size() / 2]);
+	std::vector<float> p1High(&poly1[poly1.size() / 2], &poly1[poly1.size()]);
+	std::vector<float> p2Low(&poly2[0], &poly2[poly2.size() / 2]);
+	std::vector<float> p2High(&poly2[poly2.size() / 2], &poly2[poly2.size()]);
+	std::vector<float> result(2 * poly1.size() - 1, 0);
+	auto lowLow = fourMult(p1Low, p2Low);
+	for(int i = 0; i < lowLow.size(); ++i){
+		result[i] += lowLow[i];
+	}
+	auto highHigh = fourMult(p1High, p2High);
+	for(int i = 0; i < highHigh.size(); ++i){
+		result[poly1.size() + i] += highHigh[i];
+	}
+	for(int i = 0; i < p1Low.size(); ++i){
+		p1Low[i] += p1High[i];
+		p2Low[i] += p2High[i];
+	}
+	auto temp = fourMult(p1Low, p2Low);
+	for(int i = 0; i < temp.size(); ++i){
+		temp[i] -= (lowLow[i] + highHigh[i]);
+	}
 	for(int i = 0; i < temp.size(); ++i){
 		result[poly1.size() / 2 + i] += temp[i];
 	}
